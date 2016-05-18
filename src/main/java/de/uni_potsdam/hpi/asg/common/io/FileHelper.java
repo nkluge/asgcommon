588ac173fffc6f00f3796047b141b13ef832b064
Norman Kluge <norman.kluge@hpi.de>
2016-05-18 19:16:24 +0200

    add resolver for in-install-directory files

--- src/main/java/de/uni_potsdam/hpi/asg/common/io/FileHelper.java
+++ src/main/java/de/uni_potsdam/hpi/asg/common/io/FileHelper.java
@@ -31,203 +31,219 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class FileHelper {
-	private final static Logger logger = LogManager.getLogger();
-		
-	private static FileHelper instance;
-	
-	private String workingdir;
-	
-	private FileHelper() {}
-	
-	public static FileHelper getInstance () {
-		if(instance == null) {
-			instance = new FileHelper();
-		}
-		return FileHelper.instance;
-	}
-	public void setWorkingdir(String workingdir) {
-		this.workingdir = workingdir;
-	}
-	
-	public static String getNewline() {
-		return System.getProperty("line.separator");
-	}
-	public static String getFileSeparator() {
-		return File.separator;
-	}
-	
-	public static String getFileEx(Filetype type) {
-		switch(type) {
-			case balsa:
-				return ".balsa";
-			case breeze:
-				return ".breeze";
-			case stg:
-				return ".g";
-			case verilog:
-				return ".v";
-			case log:
-				return ".log";
-			default:
-				return null;
-		}
-	}
-	
-	public boolean copyfile(File srFile, File dtFile) {
-		try {
-			// i dont know why, but sometimes FileInputStream doesnt find files with /a/b/c/../../b1
-			// with canonical path, this works
-			File newSrFile = new File(srFile.getCanonicalPath());
-			InputStream in = null;
-			OutputStream out = null;
-			try {
-				in = new FileInputStream(newSrFile);
-				out = new FileOutputStream(dtFile);
-			
-				byte[] buf = new byte[1024];
-				int len;
-				while ((len = in.read(buf)) > 0){
-					out.write(buf, 0, len);
-				}
-				return true;
-			} finally {
-				if(in != null) {
-					in.close();
-				}
-				if(out != null) {
-					out.close();
-				}
-			}	
-		} catch(IOException e) {
-			logger.error(e.getLocalizedMessage());
-			return false;
-		}
-	}
-		
-	public boolean copyfile(String srFile, String dtFile) {
-		File f1 = new File(workingdir + srFile);
-		File f2 = new File(workingdir + dtFile);
-		return copyfile(f1, f2);
-	}
-	public boolean copyfile(String srFile, File dtFile) {
-		File f1 = new File(workingdir + srFile);
-		return copyfile(f1, dtFile);
-	}
-	public boolean copyfile(File srFile, String dtFile) {
-		File f1 = new File(workingdir + dtFile);
-		return copyfile(srFile, f1);
-	}
-	
-	public enum Filetype {
-		verilog, breeze, balsa, stg, log
-	}
+    private final static Logger logger = LogManager.getLogger();
 
-	public List<String> readFile(File file) {
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-	        String line;
-	        List<String> text = new ArrayList<String>();
-	        while((line = reader.readLine()) != null) {
-	        	text.add(line);
-	        }
-	        reader.close();
-	        return text;
-		} catch(IOException e) {
-			logger.error(e.getLocalizedMessage());
-			return null;
-		}
-	}
-	
-	public List<String> readFile(String filename) {
-		File file = new File(workingdir + filename);
+    private static FileHelper   instance;
+
+    private String              workingdir;
+
+    private FileHelper() {
+    }
+
+    public static FileHelper getInstance() {
+        if(instance == null) {
+            instance = new FileHelper();
+        }
+        return FileHelper.instance;
+    }
+
+    public void setWorkingdir(String workingdir) {
+        this.workingdir = workingdir;
+    }
+
+    public static String getNewline() {
+        return System.getProperty("line.separator");
+    }
+
+    public static String getFileSeparator() {
+        return File.separator;
+    }
+
+    public static String getFileEx(Filetype type) {
+        switch(type) {
+            case balsa:
+                return ".balsa";
+            case breeze:
+                return ".breeze";
+            case stg:
+                return ".g";
+            case verilog:
+                return ".v";
+            case log:
+                return ".log";
+            default:
+                return null;
+        }
+    }
+
+    public boolean copyfile(File srFile, File dtFile) {
+        try {
+            // i dont know why, but sometimes FileInputStream doesnt find files with /a/b/c/../../b1
+            // with canonical path, this works
+            File newSrFile = new File(srFile.getCanonicalPath());
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = new FileInputStream(newSrFile);
+                out = new FileOutputStream(dtFile);
+
+                byte[] buf = new byte[1024];
+                int len;
+                while((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                return true;
+            } finally {
+                if(in != null) {
+                    in.close();
+                }
+                if(out != null) {
+                    out.close();
+                }
+            }
+        } catch(IOException e) {
+            logger.error(e.getLocalizedMessage());
+            return false;
+        }
+    }
+
+    public boolean copyfile(String srFile, String dtFile) {
+        File f1 = new File(workingdir + srFile);
+        File f2 = new File(workingdir + dtFile);
+        return copyfile(f1, f2);
+    }
+
+    public boolean copyfile(String srFile, File dtFile) {
+        File f1 = new File(workingdir + srFile);
+        return copyfile(f1, dtFile);
+    }
+
+    public boolean copyfile(File srFile, String dtFile) {
+        File f1 = new File(workingdir + dtFile);
+        return copyfile(srFile, f1);
+    }
+
+    public enum Filetype {
+        verilog, breeze, balsa, stg, log
+    }
+
+    public List<String> readFile(File file) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            List<String> text = new ArrayList<String>();
+            while((line = reader.readLine()) != null) {
+                text.add(line);
+            }
+            reader.close();
+            return text;
+        } catch(IOException e) {
+            logger.error(e.getLocalizedMessage());
+            return null;
+        }
+    }
+
+    public List<String> readFile(String filename) {
+        File file = new File(workingdir + filename);
         return readFile(file);
-	}
-	
-	public boolean writeFile(String filename, List<String> text) {
-		return writeFile(new File(workingdir + filename), text);
-	}
-	
-	public boolean writeFile(String filename, String text) {
-		return writeFile(new File(workingdir + filename), text);
-	}
-	
-	public boolean writeFile(File file, List<String> text)  {
-		StringBuilder builder = new StringBuilder();
-		for(String str : text) {
-			builder.append(str + FileHelper.getNewline()); 
-		}		
-		return writeFile(file, builder.toString());		
-	}
-	
-	public boolean writeFile(File file, String text) {
-		if(text != null) {
-			try {
-				FileWriter writer = new FileWriter(file);
-		        writer.write(text);
-		        writer.close();
-		        return true;
-			} catch(IOException e) {
-				logger.error(e.getLocalizedMessage());
-				return false;
-			}
-		} else {
-			logger.error("Nothing to write");
-			return false;
-		}
-	}
-	
-	public String mergeFileContents(List<String> filelist) {
-		try {
-			StringBuilder text = new StringBuilder();
-			List<String> modules = new ArrayList<String>();
-			for(String filename : filelist) {
-				File file = new File(workingdir + filename);
-		        BufferedReader reader = new BufferedReader(new FileReader(file));
-		        String line;
-		        while((line = reader.readLine()) != null) {
-		        	if(line.startsWith("module")) {
-		        		String modulename = line.split(" ")[1];
-		        		if(modules.contains(modulename)) {
-		        			while(!line.startsWith("endmodule")) {
-		                		line = reader.readLine();
-		                	}
-		        		} else {
-		        			modules.add(modulename);
-		        			while(!line.startsWith("endmodule")) {
-		                		text.append(line + FileHelper.getNewline());
-		                		line = reader.readLine();
-		                	}
-		        			text.append(line + FileHelper.getNewline() + FileHelper.getNewline());
-		        		}
-		        	}
-		        }
-		        reader.close();
-			}
-			
-			return text.toString();
-		} catch(IOException e) {
-			logger.error(e.getLocalizedMessage());
-			return null;
-		}
-	}
-	
-	public File newTmpFile(String name) {
-		File f = new File(workingdir + name);
-		int id = 0;
-		while(f.exists()) {
-			String split[] = name.split("\\.");
-			String newname = null;
-			if(split.length == 2) {
-				newname = workingdir + split[0] + (id++) + "." + split[1];
-			} else {
-				newname = workingdir + name + (id++);
-			}
-			f = new File(newname);
-		}
-		return f;
-	}
+    }
+
+    public boolean writeFile(String filename, List<String> text) {
+        return writeFile(new File(workingdir + filename), text);
+    }
+
+    public boolean writeFile(String filename, String text) {
+        return writeFile(new File(workingdir + filename), text);
+    }
+
+    public boolean writeFile(File file, List<String> text) {
+        StringBuilder builder = new StringBuilder();
+        for(String str : text) {
+            builder.append(str + FileHelper.getNewline());
+        }
+        return writeFile(file, builder.toString());
+    }
+
+    public boolean writeFile(File file, String text) {
+        if(text != null) {
+            try {
+                FileWriter writer = new FileWriter(file);
+                writer.write(text);
+                writer.close();
+                return true;
+            } catch(IOException e) {
+                logger.error(e.getLocalizedMessage());
+                return false;
+            }
+        } else {
+            logger.error("Nothing to write");
+            return false;
+        }
+    }
+
+    public String mergeFileContents(List<String> filelist) {
+        try {
+            StringBuilder text = new StringBuilder();
+            List<String> modules = new ArrayList<String>();
+            for(String filename : filelist) {
+                File file = new File(workingdir + filename);
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
+                while((line = reader.readLine()) != null) {
+                    if(line.startsWith("module")) {
+                        String modulename = line.split(" ")[1];
+                        if(modules.contains(modulename)) {
+                            while(!line.startsWith("endmodule")) {
+                                line = reader.readLine();
+                            }
+                        } else {
+                            modules.add(modulename);
+                            while(!line.startsWith("endmodule")) {
+                                text.append(line + FileHelper.getNewline());
+                                line = reader.readLine();
+                            }
+                            text.append(line + FileHelper.getNewline() + FileHelper.getNewline());
+                        }
+                    }
+                }
+                reader.close();
+            }
+
+            return text.toString();
+        } catch(IOException e) {
+            logger.error(e.getLocalizedMessage());
+            return null;
+        }
+    }
+
+    public File newTmpFile(String name) {
+        File f = new File(workingdir + name);
+        int id = 0;
+        while(f.exists()) {
+            String split[] = name.split("\\.");
+            String newname = null;
+            if(split.length == 2) {
+                newname = workingdir + split[0] + (id++) + "." + split[1];
+            } else {
+                newname = workingdir + name + (id++);
+            }
+            f = new File(newname);
+        }
+        return f;
+    }
+
+    public File getBasedirFile(String filename) {
+        String basedir = System.getProperty("basedir");
+        if(SystemUtils.IS_OS_WINDOWS) {
+            basedir = basedir.replaceAll("\\\\", "/");
+        }
+
+        String newfilename = basedir + getFileSeparator() + filename;
+        return new File(newfilename);
+    }
 }
